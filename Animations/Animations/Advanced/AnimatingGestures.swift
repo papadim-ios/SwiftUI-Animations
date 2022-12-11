086cdf079e0e8d2d1ee541792bddffe3dcc3efa9
Dimitris Papaioannou <dimitris.papaioannou@incrowdsports.com>
2022-12-11 11:40:33 +0200

    ShowingHidingViews

---
+++ Animations/Animations/Advanced/AnimatingGestures.swift
@@ -0,0 +1,58 @@
+//
+//  AnimatingGestures.swift
+//  Animations
+//
+//  Created by Dimitris Papaioannou on 11/12/2022.
+//
+
+import SwiftUI
+
+struct AnimatingGestures: View {
+    @State private var dragAmount = CGSize.zero
+    let letters = Array("Hello, SwiftUI")
+    @State private var enabled = false
+
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }  // translation of the drag, which tells us how far it’s moved from the start point
+                    .onEnded { _ in
+                        withAnimation {                   // Explicit animation
+                            dragAmount = .zero
+                        }
+                    }
+            )
+            .animation(.spring(), value: dragAmount)    // Implicit animation
+        
+        HStack(spacing: 0) {
+            ForEach(0 ..< letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+            // Both of those closures are given a single parameter, which describes the drag operation.
+            // Where it started, where it is currently, how far it moved
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+    }
+}
+
+struct AnimatingGestures_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatingGestures()
+    }
+}
--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -26,6 +26,22 @@ struct ContentView: View {
                 NavigationLink(destination: AnimatingBindings()) {
                     ListContents(title: "Animating Bindings", imageNumber: 4)
                 }
+                
+                NavigationLink(destination: ExplicitAnimations()) {
+                    ListContents(title: "Explicit Animations", imageNumber: 5)
+                }
+                
+                NavigationLink(destination: AnimationStack()) {
+                    ListContents(title: "Animation Stack", imageNumber: 6)
+                }
+                
+                NavigationLink(destination: AnimatingGestures()) {
+                    ListContents(title: "Animating Gestures", imageNumber: 7)
+                }
+                
+                NavigationLink(destination: ShowingHidingViews()) {
+                    ListContents(title: "Showing and Hiding Views", imageNumber: 8)
+                }
             }
             .navigationBarTitle("Animations")
         }
